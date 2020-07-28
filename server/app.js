e3a0fa6cd1d182d1c43d8703c4b33533d231ab4a
Kevin <stradtkt22@gmail.com>
2020-07-28 11:41:13 -0400

    Added the server file

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.use((req, res, next) => {
+    res.send('Hello from express');
+});
+
+module.exports = app;
