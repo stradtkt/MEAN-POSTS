00bfcf2980fa1c18bcaac50a8d0f0af86f943a26
Kevin <stradtkt22@gmail.com>
2020-07-28 21:54:32 -0400

    Added the start of mongo db displaying items from the database onto the client

--- server/app.js
+++ server/app.js
@@ -1,11 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
 const app = express();
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://kevin1234:kevin1234@cluster0.dx1q3.mongodb.net/posts_db?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save();
+    res.status(201).json({
+        message: 'Post created successfully'
+    });
+});
+
+app.get('/api/posts', (req, res, next) => {
+    Post.find()
+        .then(documents => {
+            res.status(201).json({
+                message: 'Post created successfully',
+                posts: documents
+            }); 
+        });
 });
 
 app.use('/api/posts', (req, res, next) => {
